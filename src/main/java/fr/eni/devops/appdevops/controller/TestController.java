92554cfe938464b19c75739c0cdb10da86d7ba07
Stéphane THOMAS <thomas91@free.fr>
2024-08-21 11:23:57 -0300

    TestController : Ajout Date dans l'affichage

--- src/main/java/fr/eni/devops/appdevops/controller/TestController.java
+++ src/main/java/fr/eni/devops/appdevops/controller/TestController.java
@@ -3,6 +3,7 @@ package fr.eni.devops.appdevops.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,7 @@ public class TestController {
 
         object.put("name", "Formateur");
         object.put("email", "formateur@campus-eni.fr");
-
+        object.put("Date", String.valueOf(LocalDate.now()));
         return object;
 
     }
